0df34b68a8f61ba59913a58f30ca0d56e25f1856
Jared Krajewski <jared0227@gmail.com>
2022-03-30 19:46:02 -0400

    finished warmup 2, started array 3

--- warmup 2.js
+++ warmup 2.js
@@ -293,3 +293,20 @@ function noTriples(arr) {
 }
 console.log(noTriples([1, 1, 1, 2, 2, 2, 1]));
 
+/*
+Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value, followed by the value plus 5, followed by the value minus 1. Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
+
+has271([1, 2, 7, 1]) → true
+has271([1, 2, 8, 1]) → false
+has271([2, 7, 1]) → true
+*/
+
+function has271(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] == arr[i] + 5 && arr[i + 2] == arr[i] - 1) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(has271([2, 7, 1]));
