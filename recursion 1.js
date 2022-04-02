1ef79fb5919d4a9273d3385aa02802e75700bea7
Jared Krajewski <jared0227@gmail.com>
2022-04-01 21:49:48 -0400

    recursion 1 up to sumDigits

--- recursion 1.js
+++ recursion 1.js
@@ -30,3 +30,58 @@ function bunnyEars(n) {
   }
   console.log(bunnyEars(5));
 
+/* The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+
+fibonacci(0) → 0
+fibonacci(1) → 1
+fibonacci(2) → 1
+*/
+
+function fibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(10));
+
+/* We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+bunnyEars2(0) → 0
+bunnyEars2(1) → 2
+bunnyEars2(2) → 5
+*/
+
+function bunnyEars2(n) {
+  if (n == 0) {
+    return 0;
+  }
+  return n % 2 == 0 ? 2 + bunnyEars2(n - 1) : 3 + bunnyEars2(n - 1);
+}
+
+console.log(bunnyEars2(2));
+
+/*
+We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+
+triangle(0) → 0
+triangle(1) → 1
+triangle(2) → 3
+*/
+
+function triangle(n) {
+  if (n == 0) {
+    return 0;
+  }
+  return n + triangle(n - 1);
+}
+console.log(triangle(3));
+
+/*
+Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+sumDigits(126) → 9
+sumDigits(49) → 13
+sumDigits(12) → 3
+*/
