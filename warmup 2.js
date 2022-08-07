14119e6d2436b0cb7f5718d09f4f730c4b86e3bb
Jared Krajewski <jared0227@gmail.com>
2022-08-07 08:46:58 -0700

    up to has271 with comments

--- warmup 2.js
+++ warmup 2.js
@@ -9,24 +9,31 @@ stringTimes("Hi", 1) → "Hi"
 */
 
 function stringTimes(str, n){
+  // returns the string repeated (n)*;
     return str.repeat(n)
   }
   console.log(stringTimes("Hi", 2))
 
+//////////////////////////////////////////////////////////
+
 /*
-Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+Given a string and a non-negative int n, we'll say that the front of the
+string is the first 3 chars, or whatever is there if the string is less than length 3.
+Return n copies of the front;
 frontTimes("Chocolate", 2) → "ChoCho"
 frontTimes("Chocolate", 3) → "ChoChoCho"
 frontTimes("Abc", 3) → "AbcAbcAbc" 
 */
 
 function frontTimes(str, n){
-    return str.substr(0,3).repeat(n)
-    
+  // returns the start of the string up to char 3 repeated (n)*;
+    return str.substr(0,3).repeat(n) 
   }
-  
   console.log(frontTimes("Chocolate", 2))
 
+//////////////////////////////////////////////////////////
+
+// YEAH YEAH IT WORKS! Taking Enzo out
 /*
   Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
 countXX("abcxx") → 1
@@ -35,15 +42,20 @@ countXX("xxxx") → 3
 */
 
 function countXX(str) {
-    let count = 0;
-    for (i = 0; i < str.length ; i++){
-  if (str.substr(i, + 2) == "xx")
-    count++
+  // var for count;
+  let count = 0;
+  // loop over the array 1 at a time;
+  for (i = 0; i < str.length; i++) {
+    // if the str at index i + 2 is xx increment the count and continue;
+    if (str.substr(i, +2) == "xx") count++;
   }
-    return count
-  }
-  
-  console.log(countXX("abcxx"))
+  // return the count;
+  return count;
+}
+
+console.log(countXX("abcxx"));
+
+//////////////////////////////////////////////////////////
 
 /*
 Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
@@ -53,16 +65,17 @@ doubleX("xxxxx") → true
 */
 
 function doubleX(str){
+  // loop over the string;
     for (i = 0; i < str.length; i++){
+      // if the index and the following = "xx" return true;
       if(str.substr(i , i + 1) == 'xx')
         return true;
       }
      return false;
    }
-   
    console.log(doubleX("axxbb"))
 
-
+//////////////////////////////////////////////////////////
 
 /*
 Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
@@ -71,16 +84,36 @@ stringBits("Hi") → "H"
 stringBits("Heeololeo") → "Hello" 
 */
 
+function stringBits(str) {
+  // variable with first letter of string;
+  let string = str.charAt(0);
+  // loop over the string
+  for (let i = 1; i < str.length; i++) {
+    // if i %2 and === 0 (every other element), append it to the string;
+    if (i % 2 === 0) {
+      string += str.charAt(i);
+    }
+  }
+  return string;
+}
+console.log(stringBits("Heeololeo"));
+
+//OR
+
 function stringBits(str){
+  // empty string variable;
     let result = "";
+    // loop over every other element of the string starting with the first;
     for (i = 0; i < str.length; i += 2){
+      // add the first element and every 2nd elemet to the string;
       result += str.charAt(i, i + 2)
     }
     return result;
   }
-  
   console.log(stringBits("heeololeo"))
 
+  //////////////////////////////////////////////////////////
+  
 /*
 Given a non-empty string like "Code" return a string like "CCoCodCode".
 stringSplosion("Code") → "CCoCodCode"
@@ -89,33 +122,45 @@ stringSplosion("ab") → "aab"
 */
 
 function stringSplosion(str) {
+  // variable for result;
     let result = ""
+    // loop over the array
     for (i = 0; i < str.length; i++){
+      // append the 0th index through the current index +1 to the result
       result += str.substr(0, i + 1)
     }
     return result
   }
-  
   console.log(stringSplosion("Code"))
 
+//////////////////////////////////////////////////////////
+
 /*
-Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+Given a string, return the count of the number of times that a substring length 2
+appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1
+(we won't count the end substring).
 last2("hixxhi") → 1
 last2("xaxxaxaxx") → 1
 last2("axxxaaxx") → 2 
 */
 
-function last2(str) {
-  let result = 0;
-  let last = str.substr(str.length - 2)
-  for (i = 0; i < str.length ; i++)
-    if (str.substr(i, i + 1) == last){
-     result++
-    }
-  return result
+function last2(str){
+  // variable for count;
+  let count = 0;
+  // variable for the last 2 chars of the string;
+  let last = str.substr(str.length -2,)
+  // loop over the string except for the last 2 chars;
+ for (let i = 0; i < str.length -2; i++){
+  // if the index and following 2chars equal the last 2 chars of string increment the count;
+   if (str.substr(i, 2) === last){
+     count++
+   }
+ }
+  return count;
 }
-
 console.log(last2("xaxxaxaxx"))
+
+//////////////////////////////////////////////////////////
 
 /*
 Given an array of ints, return the number of 9's in the array.
@@ -125,33 +170,42 @@ arrayCount9([1, 9, 9, 3, 9]) → 3
 */
 
 function arrayCount9(arr) {
+  // variable for count;
   let count = 0;
-
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] == 9) count++;
-  }
+  // loop over the array;
+  arr.forEach((a) => {
+    // if the index is === 9 increment the count by 1;
+    if (a === 9) {
+      count++;
+    }
+  });
   return count;
 }
-console.log(arrayCount9([1, 9, 9, 3, 9]));
+console.log(arrayCount9([1, 2, 9]));
+
+//////////////////////////////////////////////////////////
 
 /* 
-Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+The array length may be less than 4.
 arrayFront9([1, 2, 9, 3, 4]) → true
 arrayFront9([1, 2, 3, 4, 9]) → false
 arrayFront9([1, 2, 3, 4, 5]) → false 
 */
 
 function arrayFront9(arr){
-  let end = arr.length;
-  if (end > 4, end = 4);
-  for (i = 0; i < end; i ++)
-    if(arr[i] == 9){
+  // loop up to the 4th index;
+  for (let i = 0; i < 4; i++){
+    // if any index === 9 return true;
+    if (arr[i] === 9){
       return true;
-    } 
+    }
+  }
   return false;
 }
-
 console.log(arrayFront9([1, 2, 2, 3, 9]))
+
+//////////////////////////////////////////////////////////
 
 /* 
 Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
@@ -161,44 +215,51 @@ array123([1, 1, 2, 1, 2, 3]) → true
 */
 
 function array123(arr) {
-  for (i = 0; i < arr.length - 2; i++)
-    if (arr[i] == 1 && arr[i + 1] == 2 && arr[i + 2] == 3) {
-      return true;
+  // set result variable to false by default;
+  let result = false;
+  // loop over the array and if the current index 
+  arr.forEach((a) => {
+    //if the two following sequentiate 1,2,3 set result to true
+    if (arr[a] === 1 && arr[a + 1] === 2 && arr[a + 2] === 3) {
+      result = true;
     }
-  return false;
+  });
+  return result;
 }
+console.log(array123([1, 1, 2, 3, 3]));
 
-console.log(array123([1, 3, 1, 2, 3]));
+//////////////////////////////////////////////////////////
 
 /* 
-Goal: return a count of times two conescutive chars match between two strings
-params: string 1: a, string 2: b
-Return val: int (count of 2 consec chars between both strings)
-Logic: 
-
-Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+Given 2 strings, a and b, return the number of the positions where they contain
+the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since
+the "xx", "aa", and "az" substrings appear in the same place in both strings.
 stringMatch("xxcaazz", "xxbaaz") → 3
 stringMatch("abc", "abc") → 2
 stringMatch("abc", "axc") → 0 
 */ 
 
 function stringMatch(a, b) {
+  // variable to hold count;
     let count = 0;
+    // variable to find the shortest length string for iteration;
     let length = Math.min(a.length, b.length);
-  
+    // loop over the strings
     for (i = 0; i < length - 1; i++) {
+      // if 2 chars from string a match string b increment the count;
       if (a.substr(i, 2) == b.substr(i, 2)) {
         count++;
       }
     }
-  
     return count;
   }
-  
   console.log(stringMatch("xxcaazz", "xxbaaz"));
 
-  /*
-Given a string, return a version where all the "x" have been removed. Except an "x" at the very start or end should not be removed.
+//////////////////////////////////////////////////////////
+
+/*
+Given a string, return a version where all the "x" have been removed.
+Except an "x" at the very start or end should not be removed.
 
 stringX("xxHxix") → "xHix"
 stringX("abxxxcd") → "abcd"
@@ -206,20 +267,43 @@ stringX("xabxxxcdx") → "xabcdx"
 */
 
   function stringX(str) {
+    // spread the input string into an array named string;
     let string = [...str];
+    // loop over that array ignoring the first and last index;
     for (let i = 1; i < string.length - 1; i++) {
+      // if the char at i is x remove it;
       if (string[i] === "x") {
         string.splice(i, 1);
         i--;
       }
     }
-  
+  // return the array joined back as a string;
     return string.join("");
   }
   console.log(stringX("xxHxix"));
+
+  // OR
+
+  function stringX(str) {
+    // add the first char to a variable result;
+    let result = str.substr(0, 1);
+    // loop over the input string ignoring the first and last;
+    for (let i = 1; i < str.length - 1; i++) {
+      // if the index is not x, add it to the result;
+      if (str[i] != "x") {
+        result += str[i];
+      }
+    }
+    // return the result with the last char of the original string;
+    return result + str.substr(str.length - 1);
+  }
+  console.log(stringX("xxHxix"));
   
+//////////////////////////////////////////////////////////
+
 /*
-Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+Given a string, return a string made of the chars at
+indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
 
 altPairs("kitten") → "kien"
 altPairs("Chocolate") → "Chole"
@@ -227,20 +311,23 @@ altPairs("CodingHorror") → "Congrr"
 */
 
 function altPairs(str) {
+  // variable for new string;
   let string = "";
-
-  for (let i = 0; i < str.length; i++) {
-    string += str[i];
-    if (i % 2 != 0) i += 2;
+  // loop over the string 4 chars at a time;
+  for (let i = 0; i < str.length; i += 4) {
+    // add the first 2 of each set of 4 nums to the string;
+    string += str.substr(i, 2);
   }
-
   return string;
 }
+console.log(altPairs("chocolate"));
 
-console.log(altPairs("CodingHorror"));
+//////////////////////////////////////////////////////////
 
 /*
-Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+Suppose the string "yak" is unlucky. Given a string, return a version
+where all the "yak" are removed, but the "a" can be any char.
+The "yak" strings will not overlap.
 
 stringYak("yakpak") → "pak"
 stringYak("pakyak") → "pak"
@@ -248,19 +335,45 @@ stringYak("yak123ya") → "123ya"
 */
 
 function stringYak(str) {
+  // variable for new string;
+  let string = "";
+  // loop over the input string;
+  for (let i = 0; i < str.length; i++) {
+    // if the char at str[i] is "y" and the char 2 index after is "k" increment by 3;
+    if (str.charAt(i) === "y" && str.charAt(i + 2) === "k") {
+      i += 3;
+    }
+    // push each letter to the string;
+    string += str[i];
+  }
+  return string;
+}
+console.log(stringYak("yak123ya"));
+
+//or 
+
+function stringYak(str) {
+  // split the input string into an array 1 char per index;
   let string = str.split("");
+  // loop over the string array;
   for (let i = 0; i < string.length; i++) {
+    // if the char at str[i] is "y" and the char 2 index after is "k";
     if (string[i] == "y" && string[i + 2] == "k") {
+     // delete that index and the following 2;
       string.splice(i, 3);
     }
   }
+  // returns the new array as a string;
   return string.join("");
 }
 
 console.log(stringYak("yak123ya"));
 
+//////////////////////////////////////////////////////////
+
 /*
-Given an array of ints, return the number of times that two 6's are next to each other in the array. Also count instances where the second "6" is actually a 7.
+Given an array of ints, return the number of times that two 6's are
+next to each other in the array. Also count instances where the second "6" is actually a 7.
 
 array667([6, 6, 2]) → 1
 array667([6, 6, 2, 6]) → 1
@@ -269,8 +382,11 @@ array667([6, 7, 2, 6]) → 1
 
 function array667(arr) {
   let count = 0;
+  // variable for count;
   for (let i = 0; i < arr.length; i++) {
-    if ((arr[i] == 6 && arr[i + 1] === 6) || arr[i + 1] === 7) {
+    // if the current index is 6 and the following index is 6 or 7;
+    if ((arr[i] === 6 && arr[i + 1] === 6) || arr[i + 1] === 7) {
+      // increment the count;
       count++;
     }
   }
@@ -278,8 +394,11 @@ function array667(arr) {
 }
 console.log(array667([6, 7, 2, 6]));
 
+//////////////////////////////////////////////////////////
+
 /*
-Given an array of ints, we'll say that a triple is a value appearing 3 times in a row in the array. Return true if the array does not contain any triples.
+Given an array of ints, we'll say that a triple is a value appearing
+3 times in a row in the array. Return true if the array does not contain any triples.
 
 noTriples([1, 1, 2, 2, 1]) → true
 noTriples([1, 1, 2, 2, 2, 1]) → false
@@ -287,7 +406,9 @@ noTriples([1, 1, 1, 2, 2, 2, 1]) → false
 */
 
 function noTriples(arr) {
+  // loop over the array;
   for (let i = 0; i < arr.length; i++) {
+    // if the current index is equal to 1st and current index is equal to 2nd;
     if (arr[i] === arr[i + 1] && arr[i] === arr[i + 2]) {
       return false;
     }
@@ -296,8 +417,12 @@ function noTriples(arr) {
 }
 console.log(noTriples([1, 1, 1, 2, 2, 2, 1]));
 
+//////////////////////////////////////////////////////////
+
 /*
-Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value, followed by the value plus 5, followed by the value minus 1. Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
+Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value,
+followed by the value plus 5, followed by the value minus 1.
+Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
 
 has271([1, 2, 7, 1]) → true
 has271([1, 2, 8, 1]) → false
